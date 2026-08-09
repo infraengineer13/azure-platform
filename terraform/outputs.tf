d363f67e12c97a55c99d5b809d932a02786a0cab
Mateusz <swierskimateusz@gmail.com>
2026-08-09 14:54:41 +0200

    Added key_vault_id to the outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,8 @@ output "acr_login_server" {
   description = "The login server of the platform Azure Container Registry"
   value       = module.acr.login_server
 }
+
+output "key_vault_id" {
+  description = "The ID of the platform Azure Key Vault"
+  value       = module.keyvault.id
+}
